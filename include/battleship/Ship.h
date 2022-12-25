5c440d56577af044263c0ac81e3911e2b8a936c2
Antonio Tangaro <115796967+antoniotangaro@users.noreply.github.com>
2022-12-25 16:09:50 +0100

    Update Ship.h

--- include/battleship/Ship.h
+++ include/battleship/Ship.h
@@ -1,7 +1,46 @@
-#ifndef BATTLESHIP_INCLUDE_BATTLESHIP_SHIP_H_
-#define BATTLESHIP_INCLUDE_BATTLESHIP_SHIP_H_
+// Antonio Tangaro 2043429
 
-class Ship {
+#ifndef SHIP_H
+#define SHIP_H
+
+#include <string>
+
+enum class Orientation {
+  Horizontal,
+  Vertical
 };
 
-#endif//BATTLESHIP_INCLUDE_BATTLESHIP_SHIP_H_
+class Ship {
+ public:
+  // Constructor
+  Ship(std::string name, int width, int armor, Orientation orientation);
+
+  // Check if the ship is sunk (all its squares have been hit)
+  bool IsSunk();
+
+  // Take a hit
+  void TakeHit();
+
+  // Getters
+  std::string GetName();
+  int GetWidth();
+  int GetArmor();
+  int GetHits();
+  Orientation GetOrientation();
+
+  // Setters
+  void SetName(std::string name);
+  void SetWidth(int width);
+  void SetArmor(int armor);
+  void SetHits(int hits);
+  void SetOrientation(Orientation orientation);
+
+ protected:
+  std::string name_;
+  int width_;
+  int armor_;
+  int hits_;
+  Orientation orientation_;
+};
+
+#endif  // SHIP_H
