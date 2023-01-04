299ac7baeb888ece184f43c951edbd26c45171cc
Home <alessandrotrigolo@gmail.com>
2023-01-04 18:35:58 +0100

    Class Coordinates

--- src/Coordinates.cpp
+++ src/Coordinates.cpp
@@ -1,1 +1,11 @@
 #include "Coordinates.h"
+
+bool IsInBound(int min, int max){
+    return true;
+}
+
+bool GetAdjacentCoordinates(Coordinates starting, Orientation orientation, int count);
+
+static Coordinates GetCoordinates(std::string &coordinates);
+
+
