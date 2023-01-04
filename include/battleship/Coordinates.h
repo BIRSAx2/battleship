ced311158c342a1d9b61fb96ee7009e996149446
Home <alessandrotrigolo@gmail.com>
2023-01-04 15:56:46 +0100

    beginning to code Coordinates

--- include/battleship/Coordinates.h
+++ include/battleship/Coordinates.h
@@ -2,6 +2,33 @@
 #define BATTLESHIP_INCLUDE_BATTLESHIP_COORDINATES_H_
 
 class Coordinates {
+public:
+    Coordinates(int row, int col) : row_{row}, col_{col} {};
+
+    int GetRow() { return row_; }
+
+    int GetCol() { return col_; }
+
+private:
+    int row_;
+    int col_;
+
 };
 
 #endif//BATTLESHIP_INCLUDE_BATTLESHIP_COORDINATES_H_
+
+
+/*
+class Coordinates {
+ - int row_
+- int col_
++ Coordinates(int row, int column)
++ int GetRow()
++ int GetCol()
++ void SetRow(int row);
++ void SetCol(int col);
++ bool IsInBounds(int min, int max);
++ bool GetAdjacentCoordinates(Coordinates starting, Orientation orientation, int count);
++ static Coordinates(std::string &coordinates);
+}
+*/
