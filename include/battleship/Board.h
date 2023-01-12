7993164dfabbe7a2273351ad676fe3cac623bc49
Home <alessandrotrigolo@gmail.com>
2023-01-12 18:14:49 +0100

    Class Board Completed

--- include/battleship/Board.h
+++ include/battleship/Board.h
@@ -10,13 +10,15 @@ class Board {
   /// Crea un oggetto Board con rows righe e cols colonne
   /// \param rows
   /// \param cols
-  Board(int rows, int cols);
+  Board(int rows, int cols): rows_{rows}, cols_{cols} {};
 
  public:
-  int GetRows() const;
-  void SetRows(int rows);
-  int GetCols() const;
-  void SetCols(int cols);
+  int GetRows() const { return rows_; };
+
+  void SetRows(int rows) {rows_ = rows; };
+
+  int GetCols() const { return cols_; };
+  void SetCols(int cols) {cols_ = cols; }
 };
 
 #endif//BATTLESHIP_INCLUDE_BATTLESHIP_BOARD_H_
