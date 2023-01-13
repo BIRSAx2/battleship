74b1f75862db6e5c7cf12a3b52bbb05bed77e036
BIRSAx2 <mouhieddinesabir@gmail.com>
2023-01-13 18:29:15 +0100

    Added interface for handling special user commands

--- include/battleship/UserCommand.h
+++ include/battleship/UserCommand.h
@@ -13,13 +13,28 @@ enum CommandType { MOVE,
 class UserCommand {
   std::pair<Coordinates, Coordinates> move_;
   CommandType command_type_;
+  static const std::map<std::string, CommandType> SPECIAL_COMMANDS_;
 
  public:
+  /// Crea uno UserCommand a partire da un pair di Coordinates (origin, target) e un CommandType
+  /// \param move
+  /// \param command_type
   UserCommand(const std::pair<Coordinates, Coordinates> &move, CommandType command_type);
+  /// Crea uno UserCommand a partire da un CommandType
+  /// \param command_type
   explicit UserCommand(CommandType command_type);
+
+  /// Crea uno UserCommand a partire da una stringa rappresentate il comando dell'utente. Se il comando è tra quelli speciali
+  /// setta semplicemente la variabile di classe command_type_, altrimenti imposta command_type_ = MOVE e
   explicit UserCommand(const std::string &command);
+
+  /// Restituisce true se questo oggetto rappresenta un comando speciale.
   bool IsSpecial() const { return command_type_ != MOVE; };
-  static bool IsSpecial(std::string &command);
+
+  /// Verifica se il comando passato è tra quelli speciali o meno.
+  /// \param command
+  /// \return true se command è comando considerato speciale, ovvero uno tra quelli presenti come chiave in SPECIAL_COMMANDS_, false altrimenti
+  static bool IsSpecial(const std::string &command);
 };
 
 #endif//BATTAGLIA_NAVALE_INCLUDE_BATTLESHIP_USERCOMMAND_H_
