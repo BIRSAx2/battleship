9ce999f50c3e2452a078e9d4a8658c9a94473a5c
BIRSAx2 <mouhieddinesabir@gmail.com>
2023-01-17 14:11:50 +0100

    Added docs and description of design choices

--- src/Player.cpp
+++ src/Player.cpp
@@ -22,9 +22,9 @@ std::string Player::ToString() const {
   std::stringstream boards_side_by_side;
 
   boards_side_by_side << std::string((80 - name_.length()) / 2, ' ') << ColourText256(name_, 118) << "\n"
-					  // Padding per centrare il titolo della griglia di difesa
+					  // Align defence grid title
 					  << std::string((40 - defence_board_title.length()) / 2, ' ') << ColourText256(defence_board_title, 198) << std::string((40 - defence_board_title.length()) / 2, ' ')
-					  // Padding per centrare il titolo della griglia di attacco
+					  // align attack grid title
 					  << std::string((40 - attack_board_title.length()) / 2, ' ') << ColourText256(attack_board_title, 212) << "\n";
 
   while (std::getline(attack, line_from_firing_board, '\n') && std::getline(defence, line_from_game_board, '\n')) {
